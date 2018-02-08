439caaf2df5180a1eb967b5f31aca46176a93456
Alexandr Belyak <ProgrammerAlex@bk.ru>
2018-02-08 21:20:22 +0700

    Fix compilation in VS2017 and specified license type - zlib

--- PathManager.cpp
+++ PathManager.cpp
@@ -1,15 +1,36 @@
+/*
+  SimpleXlsxWriter
+  Copyright (C) 2012-2018 Pavel Akimov <oxod.pavel@gmail.com>, Alexandr Belyak <programmeralex@bk.ru>
+
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
 #include <algorithm>
 #include <cerrno>
 #include <cstring>
 #include <iostream>
 
-#include <unistd.h>
-
 #include "PathManager.hpp"
 
 #ifdef _WIN32
 #include <windows.h>
+#include <direct.h>
 #else
+#include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/syscall.h>
@@ -42,7 +63,11 @@ namespace SimpleXlsx
             remove( PathEncode( m_temp_path + ( * it ) ).c_str() );
         m_contentFiles.clear();
         for( std::vector<_tstring>::const_reverse_iterator it = m_temp_dirs.rbegin(); it != m_temp_dirs.rend(); it++ )
-            rmdir( PathEncode( ( * it ) ).c_str() );
+#ifdef _WIN32
+            _rmdir( PathEncode( ( *it ) ).c_str() );
+#else
+            rmdir( PathEncode( ( *it ) ).c_str() );
+#endif
         m_temp_dirs.clear();
     }
 
@@ -66,7 +91,7 @@ namespace SimpleXlsx
                 oldPointer = currPointer + 1;
 #ifdef _WIN32
                 std::replace( part.begin(), part.end(), _T( '/' ), _T( '\\' ) );
-                res = mkdir( PathEncode( part ).c_str() );
+                res = _mkdir( PathEncode( part ).c_str() );
 #else
                 res = mkdir( PathEncode( part ).c_str(), 0777 );
 #endif
